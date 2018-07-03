90cda4b351ca7a8ae4389abc9da03aac34303a56
Katrin Erk <katrin.erk@mail.utexas.edu>
2018-07-03 16:03:01 -0500

    webppl output that can be read back in, in a json file

--- generate_wppl.py
+++ generate_wppl.py
@@ -1,13 +1,9 @@
-# given a directory with LDC annotation about a single topic, in GAIA interface format,
-# and given a directory with the matching original LDC annotation,
-# identify all paths that link conflicting hypotheses
-# and display them, most frequent last.
+# generate webppl format output in a json file called aidagraph.json
+# that describes an AIDA graph, the units for clustering, and baseline values for cluster distances.
 #
 # usage:
-# python3 ldchypotheses.py <interchangeformatdir> <ldcdir>
+# python3 generate_wppl.py <interchangeformatdir> 
 
-#######
-# integrating LDC hypothesis annotation with the interface-format annotation
 
 import sys
 import os
@@ -19,22 +15,11 @@ from AidaGraph import AidaGraph, AidaNode
 import AnnoExplore
 import WebpplInterface
 
-indir = "shortdir"
-
-outf = open("test.wppl", "w")
+indir = sys.argv[1]
 
 mygraph = AnnoExplore.read_ldc_gaia_annotation(indir)
+wppl_obj = WebpplInterface.WpplInterface(mygraph)
 
-    
-WebpplInterface.wppl_write_graph(mygraph, outf)
-
-print("\n\n", file = outf)
-
-unit_obj = WebpplInterface.WpplUnits(mygraph)
-unit_obj.write_units(outf)
-
-print("\n\n", file = outf)
-
-unit_obj.write_unit_distances(outf)
-
+outf = open("aidagraph.json", "w")
+wppl_obj.write(outf)
 outf.close()
