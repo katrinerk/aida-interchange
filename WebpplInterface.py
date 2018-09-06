8836baa10b855c494388ede3869538d96965d1ec
Katrin Erk <katrin.erk@mail.utexas.edu>
2018-09-06 10:39:30 -0500

    debugging

--- WebpplInterface.py
+++ WebpplInterface.py
@@ -26,19 +26,15 @@ class WpplInterface:
         self.json_obj = { }
 
         # re-encode the graph
-        self.statements = [ ]
-        self.stmt_id = { }
-        
         self.json_obj["theGraph"] = self._transform_graph()
-        # get a list of statements
-        self.json_obj["statements"] = self.statements
         # and pairwise statement distances. we consider maximal distances of 5.
         self.dist = { }
         self.maxdist = 5
-        self.unreachabledist = 10 * self.maxdist
-        
+
+        # turn distance into proximity
         self.json_obj["statementProximity"] = self._compute_proximity()
 
+        # complete the entry point information given 
         self.json_obj["entrypoints"] = self._characterize_entrypoints(entrypoints)
         
     def write(self, io):
@@ -49,20 +45,21 @@ class WpplInterface:
 
     def _transform_graph(self):
         retv = { }
-        self.statements = [ ]
-        self.stmt_id = { }
         
         # we write out statements, events, entities, relations
         for node in self.mygraph.nodes():
             # entities, events: they  have a type. They also have a list of adjacent statement indices to be added later
-            if "Entity" in node.get("type", shorten = True):
-                retv[ node.shortname() ] = { "type" : "Entity" }
-            elif "Event" in node.get("type", shorten = True):
-                retv[ node.shortname() ] = { "type" : "Event" }
-            elif "Relation" in node.get("type", shorten = True):
-                retv[ node.shortname() ] = { "type" : "Relation" }
+            if node.is_entity():
+                retv[ node.shortname() ] = { "type" : "Entity",
+                                             "adjacent" : self._adjacent_statements(node)}
+            elif node.is_event():
+                retv[ node.shortname() ] = { "type" : "Event",
+                                             "adjacent" : self._adjacent_statements(node)}
+            elif node.is_relation():
+                retv[ node.shortname() ] = { "type" : "Relation",
+                                             "adjacent" : self._adjacent_statements(node)}
             # statements have a single subj, pred, obj, a maximal confidence level, and possibly mentions
-            elif "Statement" in node.get("type", shorten = True):
+            elif node.is_statement():
                 # type
                 retv[ node.shortname() ] = { "type" : "Statement"}
 
@@ -70,25 +67,14 @@ class WpplInterface:
                 for label in ["predicate", "subject", "object"]:
                     content = node.get(label, shorten = True)
                     if len(content) > 0:
-                        retv[node.shortname()][label] = str(content.pop())                        
+                        retv[node.shortname()][label] = str(content.pop())
+                
 
                 # confidence
                 conflevels = self.mygraph.confidence_of(node.name)
                 if len(conflevels) > 0:
                     retv[ node.shortname()]["conf"] = max(conflevels)
 
-                # and remember that we have seen this statement
-                self.statements.append(node.shortname())
-                self.stmt_id[ node.shortname()] = len(self.statements) - 1
-
-        # now add indices of related statements
-        # go through all ERE nodes
-        for node in self.mygraph.nodes():
-            if len(node.get("type", shorten=True).intersection({"Event", "Relation", "Entity"})) > 0:
-                retv[ node.shortname()]["adjacent"] = self._adjacent_statements(node)
-
-        #print(self.stmt_id)
-        #input("hit enter")
         return retv
 
     # for an entity, relation, or event, determine all statements that mention it
@@ -101,39 +87,33 @@ class WpplInterface:
 
                 neighbornode = self._getnode(neighbornodelabel)
                 if neighbornode is not None:
-                    if "Statement" in neighbornode.get("type", shorten = True):
-                        retv.append(self.stmt_id[neighbornode.shortname()])
+                    if neighbornode.is_statement():
+                        retv.append(neighbornode.shortname())
                         
         return retv
     
 
     # compute the proximity between statements.
-    # return as a list of lists,
-    # where list i contains the proximity of statement i to all other statements, normalized to sum to 1.
-    # 
+    # return as a dictionary mapping entry indices to dictionaries other_entry_index:proximity
+    # proximities are normalized to sum to 1.
     def _compute_proximity(self):
         # get pairwise node distances
+        # in the shape of a dictionary
+        # self.dist: node label -> node label -> distance
         self._compute_node_distances()
 
-        # compute unit distances
-        retv = [ ]
+        # compute unit proximity
+        retv = { }
         
-        for i in range(len(self.json_obj["statements"])):
-            distances = [ self.getdist(self.statements[i], self.statements[j]) for j in range(len(self.json_obj["statements"]))]
-            # mark node unreachable from itself
-            distances[i] = self.unreachabledist
-            
+        for stmt1 in self.dist.keys():
             # sum of proximities for reachable nodes
-            sumprox = sum(self.maxdist - distances[j] for j in range(len(distances)) if distances[j] < self.unreachabledist)
+            sumprox = sum(self.maxdist - dist for dist in self.dist[stmt1].values())
 
             # proximity normalized by summed proximities
             if sumprox > 0:
-                # this node is actually reachable from somewhere
-                retv.append([(self.maxdist - distances[j])/sumprox if distances[j] < self.unreachabledist else 0.0 \
-                                for j in range(len(distances))])
-            else:
-                # this node is unconnected to everything. It has normalized proximities of zero to everything.
-                retv.append([0.0] * len(distances))
+                retv[stmt1] = { }
+                for stmt2 in self.dist[stmt1].keys():
+                    retv[stmt1][stmt2] = (self.maxdist - self.dist[stmt1][stmt2]) / sumprox
 
         return retv
  
@@ -143,20 +123,21 @@ class WpplInterface:
     # don't use Floyd-Warshall, it's too slow with this many nodes
     def _compute_node_distances(self):
         # target data structure:
-        # (nodename1, nodename2) -> distance
-        # where nodename1 is alphabetically before nodename2
+        # nodename1 -> nodename2 -> distance
         self.dist = { }
 
         # we only do statement nodes.
-        labels = [ k for k, n in self.mygraph.node.items() if "Statement" in n.get("type", shorten = True)]
-        # we step through neighbors that are event or entities too
-        visitlabels = set([ k for k, n in self.mygraph.node.items() if len(n.get("type", shorten=True).intersection({"Statement", "Event", "Relation", "Entity"})) > 0])
+        self.statements = [ k for k, n in self.mygraph.node_dict.items() if n.is_statement()]
+        # we step through neighbors that are EREs or statements
+        visitlabels = set([ k for k, n in self.mygraph.node_dict.items() if n.is_statement() or n.is_ere()])
 
         # do maxdist steps outwards from each statement node
-        for subj in labels:
+        for subj in self.statements:
             subjnode = self._getnode(subj)
             if subjnode is None: continue
-                
+
+            subjlabel = subjnode.shortname()
+            
             # next round of nodes to visit: neighbors of subj
             fringe = self._valid_neighbors(subjnode, visitlabels)
 
@@ -167,28 +148,33 @@ class WpplInterface:
                     if obj == subj: continue
                     objnode = self._getnode(obj)
                     if objnode is None: continue
-                        
-                    self.dist[ self._nodepair(subjnode.shortname(), objnode.shortname())] = \
-                      min( self.getdist(subjnode.shortname(), objnode.shortname()), dist)
+
+                    objlabel = objnode.shortname()
+                    
+                    if objnode.is_statement():
+                        # keep track of distance only if this is a statement node
+                        if subjlabel not in self.dist: self.dist[subjlabel] = { }
+                        self.dist[subjlabel][objlabel] = min(self.dist[subjlabel].get(objlabel, self.maxdist + 1), dist)
+                    
                     newfringe.update(self._valid_neighbors(objnode, visitlabels))
                 fringe = newfringe
                 dist += 1
 
                 
 
-    # helper functions for node_distances. called by get_node_distances and _unit_distance
-    def getdist(self, l1, l2):
-        if l1 == l2:
-            return 0
-        elif l1 < l2 and (l1, l2) in self.dist:
-            return self.dist[ (l1, l2) ]
-        elif l2 < l1 and (l2, l1) in self.dist:
-            return self.dist[ (l2, l1) ]
-        else: return self.unreachabledist
+    # # helper functions for node_distances. called by get_node_distances and _unit_distance
+    # def getdist(self, l1, l2):
+    #     if l1 == l2:
+    #         return 0
+    #     elif l1 < l2 and (l1, l2) in self.dist:
+    #         return self.dist[ (l1, l2) ]
+    #     elif l2 < l1 and (l2, l1) in self.dist:
+    #         return self.dist[ (l2, l1) ]
+    #     else: return self.unreachabledist
 
-    # sorted pair of label 1, label 2
-    def _nodepair(self, l1, l2):
-        return tuple(sorted([l1, l2]))
+    # # sorted pair of label 1, label 2
+    # def _nodepair(self, l1, l2):
+    #     return tuple(sorted([l1, l2]))
 
     # return all node labels that have an incoming or outgoing edge to/from the node with label nodelabel
     def _valid_neighbors(self, node, visitlabels):
@@ -215,9 +201,6 @@ class WpplInterface:
     def _characterize_entrypoints(self, entrypoints):
         for entrypoint in entrypoints:
 
-            # replace statements by their IDs
-            entrypoint["statements"] = [ self.stmt_id[ s] for s in entrypoint["statements"]]
-            
             # fill in candidates            
             candidateset = set()
 
@@ -236,7 +219,7 @@ class WpplInterface:
         return entrypoints
 
     def _getnode(self, nodelabel):
-        if nodelabel in self.mygraph.node:
-            return self.mygraph.node[nodelabel]
+        if nodelabel in self.mygraph.node_dict:
+            return self.mygraph.node_dict[nodelabel]
         else: return None
          
--- generate_wppl.py
+++ generate_wppl.py
@@ -22,13 +22,15 @@ entrypoints = [{
     "ere" : ["E780874.00294", "V780874.00065"],
     # MH-17 is-a Vehicle, kbEntry MH-17, the event is an attack, target-of Conflict-Attack (repeated).
     "statements" : [ "assertion-3434",  "assertion-3455", "assertion-3459", "assertion-3463", "assertion-3466"],
-    "corefacetLabels" : ["?crash_target", "?crash_event"],
-    "corefacetFillers" : ["E780874.00294","V780874.00065"],
-    "coreconstraints" : [
-        ["?crash_event", "Conflict_attack_attacker", "?crash_attacker"],
-        ["?crash_event", "Conflict_attack_instrument", "?crash_instrument"],
-        ["?crash_event", "Conflict_attack_place", "?crash_place"]
+    "core" : {
+        "facetLabels" : ["?crash_target", "?crash_event"],
+        "facetFillers" : ["E780874.00294","V780874.00065"],
+        "constraints" : [
+        ["?crash_event", "Conflict.Attack_Attacker", "?crash_attacker"],
+        ["?crash_event", "Conflict.Attack_Instrument", "?crash_instrument"],
+        ["?crash_event", "Conflict.Attack_Place", "?crash_place"]
     ],
+    }
 }]
 
 g = rdflib.Graph()
