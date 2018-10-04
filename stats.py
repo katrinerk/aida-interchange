96b7208fcf2d74ddcb1d6072d7bdc327fcd275e1
Pengxiang Cheng <pxcheng@utexas.edu>
2018-10-04 17:50:13 -0500

    run simple stats over ttl file

---
+++ stats.py
@@ -0,0 +1,27 @@
+import rdflib
+import sys
+from AidaGraph import AidaGraph
+
+input_path = sys.argv[1]
+
+print('Reading ttl file from {}...'.format(input_path))
+g = rdflib.Graph()
+g.parse(input_path, format='ttl')
+print('Done.')
+
+print('Found {} triples.'.format(len(g)))
+
+print('Adding all triples to AidaGraph...')
+mygraph = AidaGraph()
+mygraph.add_graph(g)
+print('Done.')
+
+print('Printing statistics...')
+print('Found {} nodes.'.format(len(list(mygraph.nodes()))))
+print('Found {} Entity nodes.'.format(len(list(mygraph.nodes('Entity')))))
+print('Found {} Relation nodes.'.format(len(list(mygraph.nodes('Relation')))))
+print('Found {} Event nodes.'.format(len(list(mygraph.nodes('Event')))))
+print('Found {} Statement nodes.'.format(len(list(mygraph.nodes('Statement')))))
+print('Found {} SameAsCluster nodes.'.format(len(list(mygraph.nodes('SameAsCluster')))))
+print('Found {} ClusterMembership nodes.'.format(len(list(mygraph.nodes('ClusterMembership')))))
+
