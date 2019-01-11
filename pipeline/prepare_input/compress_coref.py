6deb2d8a09106b7cd2030656fbc3567a0c1c9529
Katrin Erk <katrin.erk@mail.utexas.edu>
2019-01-11 10:44:22 -0600

    added comments to the EREUnifier data structure

--- pipeline/prepare_input/compress_coref.py
+++ pipeline/prepare_input/compress_coref.py
@@ -17,29 +17,46 @@ import json
 import random
 from argparse import ArgumentParser
 
-
+# data structure for managing coreference
+# by having a single representative "unifier" for each
+# coreference group.
 class EREUnify:
     def __init__(self):
         self.unifier = { }
 
+    # ere: a string label for an ERE (entity, relation, event)
+    # add to the data structure.
     def add(self, ere):
         if ere not in self.unifier:
             self.unifier[ere] = ere
-            
+
+    # ere: a string label for an ERE
+    # coref: representative unifier
+    # make it so that the unifier of ere is coref.
+    # any EREs that have 'ere' as their representative unifier
+    # will be made to point to 'coref' instead.
     def unify_ere_coref(self, ere, coref):
         ereu = self.get_unifier(ere)
         if ereu != coref:
             # unification needed
             self.unifier[ ere] = coref
-            # if any other variable points to 'othervar', make it point to 'unifier'
+            # if any other variable points to 'ere', make it point to 'coref'
             for var in self.unifier.keys():
                 if self.unifier[var] == ere:
                     self.unifier[var] = coref
 
 
+    # ell: string label for an ERE
+    # returns the representative unifier for ell.
+    # if ell is not in the data structure, it is assumed to point to itself.
     def get_unifier(self, ell):
         return self.unifier.get(ell, ell)
 
+    # make a new datastructure like self.unifier
+    # that maps all EREs to new unifiers.
+    # the new data structure is a dictionary.
+    # its keys are the same EREs that are listed in self.unifier.
+    # the representative unifiers that are the values are all new. 
     def all_new_names(self):
         retv = { }
         oldname_newname = { }
